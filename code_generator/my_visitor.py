f0f4da5b2396a0d455648b930e820a34c6bb3096
OstrozhinskayaIrina <airolimp@mail.ru>
2024-10-04 00:03:36 +0300

    Added conditions

--- code_generator/my_visitor.py
+++ code_generator/my_visitor.py
@@ -22,7 +22,6 @@ class LuaGenerator(LuaVisitor):
     def __init__(self, error_listener=LuaErrorListener()):
         self.module = ir.Module()
         llvm_function = ir.Function(self.module, ir.FunctionType(ir.IntType(32), []), name="main")
-        #llvm_function = ir.Function(self.module, ir.FunctionType(ir.VoidType(), []), name="main")
         self.builder = ir.IRBuilder(llvm_function.append_basic_block(name="main"))
         self.symbol_table = SymbolTable()
 
@@ -52,8 +51,6 @@ class LuaGenerator(LuaVisitor):
     
     def visitStat(self, ctx: LuaParser.StatContext):
         print('Stat')
-        #self.builder.append_basic_block(self.builder.basic_block)
-        print(self.builder.basic_block)
         if match_rule(ctx.children[0], LuaParser.RULE_varlist1):
             # посещение правила varlist1
             lhs, lhs_ptr, name = self.visit(ctx.varlist1())
@@ -70,7 +67,31 @@ class LuaGenerator(LuaVisitor):
             converted_rhs = LuaTypes.cast_type(self.builder, value=rhs, target_type=lhs_ptr.type.pointee, ctx=ctx)
             
             self.builder.store(value=converted_rhs, ptr=lhs_ptr)
-            return converted_rhs, None
+            #return converted_rhs, None
+        
+        #если у нас if - else
+        elif ctx.children[0].getText() == 'if':
+            blocks = ctx.block()
+            exps = ctx.exp()
+
+            cond_val, _ = self.visit(exps[0])
+            converted_cond_val = LuaTypes.cast_type(self.builder, target_type=LuaTypes.bool, value=cond_val, ctx=ctx)
+            
+            if ctx.children[-2].getText() == 'else':
+                with self.builder.if_else(converted_cond_val) as (then, otherwise):
+                    with then:
+                        self.symbol_table.enter_scope()
+                        self.visit(blocks[0])
+                        self.symbol_table.exit_scope()
+                    with otherwise:
+                        self.symbol_table.enter_scope()
+                        self.visit(blocks[-1])
+                        self.symbol_table.exit_scope()
+            else:
+                with self.builder.if_then(converted_cond_val):
+                    self.symbol_table.enter_scope()
+                    self.visit(blocks[0])
+                    self.symbol_table.exit_scope()
         
     def visitVarlist1(self, ctx: LuaParser.Varlist1Context):
         var_val, var, name = self.visit(ctx.var(0))
@@ -95,8 +116,11 @@ class LuaGenerator(LuaVisitor):
             val, type = self.visit(ctx.number())
         elif ctx.string() is not None:
             val, type = self.visit(ctx.string())
+        elif ctx.prefixexp() is not None:
+            val, type = self.visit(ctx.prefixexp())
         elif ctx.getText() is not None:
             text = ctx.getText()
+            print('TEXT!', text)
             if text == 'nil':
                 val = None
                 type = ir.VoidType()
@@ -106,6 +130,21 @@ class LuaGenerator(LuaVisitor):
             elif text == 'false':
                 val = ir.Constant(ir.IntType(1), 0)
                 type = val.type
+            
+        elif ctx.tableconstructor() is not None:
+            val, type = self.visit(ctx.string())
+        return val, type
+
+    def visitPrefixexp(self, ctx: LuaParser.PrefixexpContext):
+        val, type = self.visit(ctx.varOrExp())
+        return val, type
+
+    def visitVarOrExp(self, ctx: LuaParser.VarOrExpContext):
+        if ctx.var() is not None:
+            val, var, _ = self.visit(ctx.var())
+            type = var.type
+        if ctx.exp() is not None:
+            val, type = self.visit(ctx.exp())
         return val, type
     
     def visitNumber(self, ctx: LuaParser.NumberContext):
---
+++ samples/3.lua
@@ -0,0 +1,10 @@
+b = false
+if true then
+    a = 3
+else
+    c = 4
+end 
+
+if b then
+    d = 5
+end
